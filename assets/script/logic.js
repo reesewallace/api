115faa06fa0ba3be5749eac172e842c08e8dfdd6
Students <students@HEA-0A-00207-04.local>
2018-04-12 19:46:36 -0400

    7:46 p.m.

--- assets/script/logic.js
+++ assets/script/logic.js
@@ -5,20 +5,21 @@ $(document).ready(function(){
 var movie = "batman";
 
 var URL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=dc6zaTOxFJmzC&limit=10";
-var random = Math.random() * 9 + 1;
-random = Math.floor(random);
+
 
 var movies = [];
 $.get(URL, function(response){
   console.log(response);
 
-$("#append").append("<img src=" + response.data[random].images.original.url + " alt=" + response.data[0].title + ">")
-$("#append").append("<h1>This gif is rated: " + response.data[random].rating + " </h1>")
+$("#append").append('<img class="gif" src="' + response.data[0].images.original.url + '" alt=" + response.data[0].title + ">')
+$('.btn0').click(function() {
 
-});
-
-
+alert(response.data[0].title);
 
 
+        });
+
+
+    });
 
 });
